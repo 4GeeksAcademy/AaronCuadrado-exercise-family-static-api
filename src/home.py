a0993d1e81c861abe49f3ca19cc79c3384f137d2
Aaron Cuadrado <163054420+AaronCuadrado@users.noreply.github.com>
2024-09-10 14:18:57 +0000

    fin

---
+++ src/home.py
@@ -0,0 +1,13 @@
+lista_de_compra = ['platanos', 'fresas', 'fresas']
+print(lista_de_compra)
+print(set(lista_de_compra))
+
+diccionario = {
+    'color': 'azul',
+    'colores': []
+}
+
+diccionario['color']
+print(diccionario['color'])
+print(diccionario.get('colores', 'no existe'))
+
